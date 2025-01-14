f748214d1732931d0d5efeb0d331159e21c0c68a
Devi <devidekshina7@gmail.com>
2025-01-15 00:16:07 +0530

    Frontend and backend

---
+++ backend/src/lib/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGOOSE_URI);
+    console.log(`Mongoose connected: ${(conn, connection.host)}`);
+  } catch (error) {}
+};
